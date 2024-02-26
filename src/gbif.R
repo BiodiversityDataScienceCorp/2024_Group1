18135aed272a6a5846ce9e91093c12797c129549
kacielau <lc21-0648@lclark.edu>
2024-02-26 17:59:44 +0000

    cleaned data

--- src/gbif.R
+++ src/gbif.R
@@ -1,20 +1,30 @@
+# list of packages
+
 packages<-c("tidyverse", "rgbif", "usethis", "CoordinateCleaner", "leaflet", "mapview")
 
+# install packages
 
 installed_packages<-packages %in% rownames(installed.packages())
 if(any(installed_packages==FALSE)){
   install.packages(packages[!installed_packages])
 }
 
+# loading packages through library function
 
 invisible(lapply(packages, library, character.only=TRUE))
 
 usethis::edit_r_environ()
 
+# setting up data from GBIF
+
 voleBackbone<-name_backbone(name="Arborimus longicaudus")
 speciesKey<-voleBackbone$usageKey
 
+# download data in csv format
+
 occ_download(pred("taxonKey", speciesKey), format="SIMPLE_CSV")
+
+# Citation Info:
 
 #<<gbif download>>
 #  Your download is being processed by GBIF:
@@ -39,8 +49,34 @@ occ_download(pred("taxonKey", speciesKey), format="SIMPLE_CSV")
 #Citation:
 #  GBIF Occurrence Download https://doi.org/10.15468/dl.ga9kke Accessed from R via rgbif (https://github.com/ropensci/rgbif) on 2024-02-25
 
+# download raw data
 
 d <- occ_download_get('0017170-240216155721649', path="data/") %>%
   occ_download_import()
 write_csv(d, "data/rawData.csv")
 
+#cleaning data
+fData<-d %>%
+  filter(!is.na(decimalLatitude),!is.na(decimalLongitude))
+
+fData<-fData %>%
+  filter(countryCode %in% c("US", "CA", "MX"))
+
+fData<-fData %>%
+  filter(!basisOfRecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))
+
+fData<-fData %>%
+  cc_sea(lon="decimalLongitude", lat="decimalLatitude")
+
+#remove duplicates
+fData<-fData %>%
+  distinct(decimalLongitude, decimalLatitude, speciesKey, datasetKey, .keep_all = TRUE)
+
+#one fell swoop
+#  filter(!is.na(decimalLatitude),!is.na(decimalLongitude))
+#  filter(countryCode %in% c("US", "CA", "MX"))
+#  filter(!basisOfRecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))
+#  cc_sea(lon="decimalLongitude", lat="decimalLatitude")
+#  distinct(decimalLongitude, decimalLatitude, speciesKey, datasetKey, .keep_all = TRUE)
+
+write_csv(fData, "data/cleanedData.csv")
