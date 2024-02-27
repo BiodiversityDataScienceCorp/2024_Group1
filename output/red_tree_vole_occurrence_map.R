7ca93213372753993c9ed220a420fe8e70495671
el-son <lc23-0969@lclark.edu>
2024-02-27 04:41:25 +0000

    wrote code for the map, but having issues running it

---
+++ output/red_tree_vole_occurrence_map.R
@@ -0,0 +1,35 @@
+
+install.packages("leaflet")
+library(leaflet)
+
+install.packages("mapview")
+library(mapview)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("magrittr")
+library(magrittr)
+
+
+data <- read.csv("data/cleanedData.csv")
+
+
+
+
+map <- leaflet() %>% 
+  addProviderTiles("Esri.WorldTopoMap") %>% 
+  addCircleMarkers(data = data,
+                   lat = ~decimalLatitude,
+                   lng = ~decimalLongitude,
+                   radius = 3,
+                   color = "maroon",
+                   fill = 0.3) %>% 
+  addLegend(position = "topright",
+            title = "Species Occurrences",
+            labels = "Red tree vole",
+            colors = "maroon",
+            opacity = 0.3)
+
+
+
