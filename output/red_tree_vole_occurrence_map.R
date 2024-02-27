eed0e550b42eee92ff660cb8b63b79c3d22e2156
el-son <lc23-0969@lclark.edu>
2024-02-27 20:49:04 +0000

    generated the map and took a screenshot

--- output/red_tree_vole_occurrence_map.R
+++ output/red_tree_vole_occurrence_map.R
@@ -18,20 +18,48 @@ data <- read.csv("data/cleanedData.csv")
 
 
 # create map
-
-map<- leaflet() %>%
-  addProviderTiles("Esri.WorldTopoMap") %>%
+map <- leaflet() |>
+  addProviderTiles("Esri.WorldTopoMap") |>
   addCircleMarkers(data = data,
                    lat = ~decimalLatitude,
                    lng = ~decimalLongitude,
                    radius = 3,
                    color = "maroon",
-                   fill = 0.3) %>%
+                   fill = 0.3) |>
   addLegend(position = "topright",
-            title = "Species Occurrences",
-            labels = "Red tree vole",
+            title = "Species Occurences",
+            labels = "Red Tree Vole",
             colors = "maroon",
             opacity = 0.3)
+
+
+#made the map
+map |>
+  addProviderTiles("Esri.WorldTopoMap") |>
+  addCircleMarkers(data = data,
+                   lat = ~decimalLatitude,
+                   lng = ~decimalLongitude,
+                   radius = 3,
+                   color = "pink",
+                   fill = 0.3) |>
+  addLegend(position = "topright",
+            title = "Species Occurences",
+            labels = "Red Tree Vole",
+            colors = "pink",
+            opacity = 0.3)
+
+
+
+
+#save the map
+install.packages("webshot2")
+library(webshot2)
+
+
+mapshot2(map, file = "output/leafletTest.png")
+
+
+
 
 
 
